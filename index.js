0ffdffa1fec634e5a8f3290e896fa8bd0701309c
Vatsala Singh <45648592+vatsala-singh@users.noreply.github.com>
2021-03-13 18:33:28 +0530

    JS tutorial complete

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// console.log('HEllo World');
+
+// let name = 'Vatsala';
+// console.log(name);
+
+// let interest = 0.3;
+// interest = 1;
+// console.log(interest);
+// //output 1
+
+// const interest = 0.3;
+// interest = 1;
+// console.log(interest);
+// //index.js:13 Uncaught TypeError: Assignment to constant variable.
+// //at index.js: 13
+//   //  (anonymous) @index.js: 13
+
+
+// let name = 'Vatsala';
+// let age = 22;
+// let isApproved =false;
+
+//person object
+
+// let person = {
+//     name: 'Vatsala', //key avlue pair
+//     age: 30
+
+// };//object literal
+
+//change te name of a property using dot
+// person.name='Varu';
+// //using [] notations
+// person['name'] = 'Preeti';
+
+// console.log(person);
+
+let sc =['red','blue'];
+sc[2] = 1;
+console.log(sc);
+console.log(sc.length)
